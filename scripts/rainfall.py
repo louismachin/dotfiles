50d265d77d8a689566be23b5a0a01ebfd95e8ed5
Louis Machin <ldhmachin@gmail.com>
2023-10-19 16:04:36 +0100

    Updated dotfiles

--- scripts/rainfall.py
+++ scripts/rainfall.py
@@ -1,68 +1,144 @@
-"""
-===============
-Rain simulation
-===============
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-Simulates rain drops on a surface by animating the scale and opacity
-of 50 scatter points.
-
-Author: Nicolas P. Rougier
-"""
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import time
+import random
+import os
+import sys
 
 
-# Create new Figure and an Axes which fills it.
-fig = plt.figure(figsize=(7, 7))
-ax = fig.add_axes([0, 0, 1, 1], frameon=False)
-ax.set_xlim(0, 1), ax.set_xticks([])
-ax.set_ylim(0, 1), ax.set_yticks([])
+colors = {
+    "black": "\u001b[30m",
+    "red": "\u001b[31m",
+    "green": "\u001b[32m",
+    "yellow": "\u001b[33m",
+    "blue": "\u001b[34m",
+    "magenta": "\u001b[35m",
+    "cyan": "\u001b[36m",
+    "white": "\u001b[37m",
+    "reset": "\u001b[0m",
 
-# Create rain data
-n_drops = 50
-rain_drops = np.zeros(n_drops, dtype=[('position', float, 2),
-                                      ('size',     float, 1),
-                                      ('growth',   float, 1),
-                                      ('color',    float, 4)])
+    "b_black": "\u001b[30;1m",
+    "b_red": "\u001b[31;1m",
+    "b_green": "\u001b[32;1m",
+    "b_yellow": "\u001b[33;1m",
+    "b_blue": "\u001b[34;1m",
+    "b_magenta": "\u001b[35;1m",
+    "b_cyan": "\u001b[36;1m",
+    "b_white": "\u001b[37;1m",
 
-# Initialize the raindrops in random positions and with
-# random growth rates.
-rain_drops['position'] = np.random.uniform(0, 1, (n_drops, 2))
-rain_drops['growth'] = np.random.uniform(50, 200, n_drops)
+    "Reset": "\u001b[0m",
+}
 
-# Construct the scatter which we will update during animation
-# as the raindrops develop.
-scat = ax.scatter(rain_drops['position'][:, 0], rain_drops['position'][:, 1],
-                  s=rain_drops['size'], lw=0.5, edgecolors=rain_drops['color'],
-                  facecolors='none')
-
-
-def update(frame_number):
-    # Get an index which we can use to re-spawn the oldest raindrop.
-    current_index = frame_number % n_drops
-
-    # Make all colors more transparent as time progresses.
-    rain_drops['color'][:, 3] -= 1.0/len(rain_drops)
-    rain_drops['color'][:, 3] = np.clip(rain_drops['color'][:, 3], 0, 1)
-
-    # Make all circles bigger.
-    rain_drops['size'] += rain_drops['growth']
-
-    # Pick a new position for oldest rain drop, resetting its size,
-    # color and growth factor.
-    rain_drops['position'][current_index] = np.random.uniform(0, 1, 2)
-    rain_drops['size'][current_index] = 5
-    rain_drops['color'][current_index] = (0, 0, 0, 1)
-    rain_drops['growth'][current_index] = np.random.uniform(50, 200)
-
-    # Update the scatter collection, with the new colors, sizes and positions.
-    scat.set_edgecolors(rain_drops['color'])
-    scat.set_sizes(rain_drops['size'])
-    scat.set_offsets(rain_drops['position'])
+def Colored(string, color):
+    return string if "-m" in sys.argv else ( colors[color] + string + colors["Reset"])
 
 
-# Construct the animation, using the update function as the animation
-# director.
-animation = FuncAnimation(fig, update, interval=10)
-plt.show()
+def Clear_Screen():
+    print("\033[2J") # erase saved lines
+    print("\033[3J") # erase entire screen
+    print("\033[H") # moves cursor to home position
+
+
+def Get_Arguments():
+    args_dict = {"colors": []}
+    if sys.argv:
+        for arg in sys.argv:
+            if "-i=" in arg:
+                args_dict["intensity"] = int(arg.split("-i=")[1])
+            if "-t=" in arg:
+                args_dict["timing"] = float(arg.split("-t=")[1])
+            if arg in colors:
+                args_dict["colors"].append(arg)
+    if not args_dict["colors"]:
+        del args_dict["colors"]
+    return args_dict
+
+
+def New_Drop():
+    for i in range(intensity):
+        shape = random.choice(DROPSHAPES)
+        color = random.choice(drop_colors)
+
+        raindrop = {
+            "shape": Colored(shape, color),
+            "x": random.randint(0, xmax),
+            "y": 0,
+        }
+        rainfall.append(raindrop)
+
+def Rain():
+    ## iterate over every line
+    for i in range(ymax):
+        line = " "*xmax
+
+        ### to avoid splicing of ansi codes, splice in the drops from the end of the line
+        this_line_raindrops = [raindrop for raindrop in rainfall if raindrop["y"] == i]
+        this_line_raindrops.sort(key=lambda y: y["x"])
+        this_line_raindrops.reverse()
+
+        ## insert new drops and shift existing drops
+        for raindrop in this_line_raindrops:
+            x = raindrop["x"]
+            line = line[:x] + raindrop["shape"] +line[x:]
+
+        print(line)
+
+    ### update raindrop positions
+    for raindrop in rainfall:
+        raindrop["y"] += 1
+
+        ## once a raindrop reaches the ground, they splash
+        if raindrop["y"] > ymax-2:
+            raindrop["shape"] = Colored("o", random.choice(drop_colors))
+
+        # raindrops outside the window evaporate
+        if raindrop["y"] > ymax:
+            rainfall.remove(raindrop)
+
+    New_Drop()
+
+
+
+def Weather_Forecast():
+    global weather
+    global intensity
+
+    weather += 1
+    if weather == 100:
+        weather = 0
+        intensity += random.choice([-1,1])
+        if intensity < 1:
+            intensity = 1
+        if intensity > 10:
+            intensity = 10
+
+
+size = os.get_terminal_size()
+xmax = size.columns
+ymax = int(size.lines)
+
+weather = 0
+rainfall = []
+DROPSHAPES =["|", "│", "┃", "╽", "╿", "║", "┆", "┇", "┊", "┋", "╵", "╹", "╻"]
+
+args = Get_Arguments()
+intensity = args.get("intensity", 1)
+timing = args.get("timing", 0.08)
+drop_colors = args.get("colors", ["blue", "b_blue"])
+
+
+
+print('\033[?25l', end="") ## hides the cursor
+New_Drop()
+
+try:
+    while True:
+        Rain()
+        time.sleep(timing)
+        Clear_Screen()
+        Weather_Forecast()
+
+except KeyboardInterrupt:
+    Clear_Screen()
+    print('\033[?25h', end="") # makes cursor visible again
