a8ccbd725fef03c4ab1b08b92ca73e4e2670a449
Louis Machin <ldhmachin@gmail.com>
2023-10-19 15:27:24 +0100

    Updated motd.rb

--- scripts/motd.rb
+++ scripts/motd.rb
@@ -13,62 +13,31 @@ class String
     def titleize
         return split(/ |\_|\-/).map(&:capitalize).join(" ") 
     end
-end
 
-def get_http_data(url)
-    begin
-        uri = URI.parse(url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl, http.verify_mode = true, OpenSSL::SSL::VERIFY_NONE
-        request = Net::HTTP::Get.new(url)
-        response = http.request(request)
-        return JSON.parse(response.body) if response.code == "200"
-        raise StandardError.new
-    rescue => e
-        return {}
+    def italics
+        return "\e[3m#{self}\e[23m"
     end
 end
 
-def get_platform
-    str = RUBY_PLATFORM
-    if File.exists?('/etc/lsb-release')
-        File.open('/etc/lsb-release', 'r').read.each_line do |line|
-            if line.include?('DISTRIB_DESCRIPTION')
-                str = line.chomp.gsub('DISTRIB_DESCRIPTION=', '').gsub('"', '')
-            end
-        end
-    end
-    return "Welcome to #{str}!"
+def get(url, params = {}, headers = {})
+    uri = URI(url)
+    uri.query = URI.encode_www_form(params) unless params == {}
+    req = Net::HTTP::Get.new(uri)
+    headers.each { |key, value| req[key] = value }
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http|
+        http.request(req)
+    }
+    return res
 end
 
-
-
-def get_logo
-    lines = []
-    lines << "  ▄▄▄▄▄ ▄▄▄▄▄ ▄▄▄▄▄ ▄   ▄ ▄▄▄▄▄ ▄▄  ▄   ▄▄▄▄  ▄▄▄▄▄ ▄   ▄"
-    lines << "  █ █ █ █   █ █   █ █   █   █   █ █ █   █   █ █     █   █"
-    lines << "  █ █ █ █▄▄▄█ █     █▄▄▄█   █   █  ██   █   █ █▄▄▄▄  █ █ "
-    lines << "  █   █ █   █ █▄▄▄▄ █   █ ▄▄█▄▄ █   █ ▄ █▄▄▄█ █▄▄▄▄  █▄█ "
-    colors = [:white, :dark_white, :dark_black]
-    return lines.map.with_index { |s, i| s.colorize(colors[i]) }.join("\n")
-end
-
-def old_get_logo
-    lines = []
-    lines << "                            __    _            __         "
-    lines << "      ____ ___  ____ ______/ /_  (_)___   ____/ /__ _   __"
-    lines << "     / __ `__ \\/ __ `/ ___/ __ \\/ / __ \\ / __  / _ \\ | / /"
-    lines << "    / / / / / / /_/ / /__/ / / / / / / // /_/ /  __/ |/ / "
-    lines << "   /_/ /_/ /_/\\__,_/\\___/_/ /_/_/_/ /_(_)__,_/\\___/|___/  "
-    colors = [:white, :dark_white, :dark_black, :light_black, :green]
-    return lines.map.with_index { |s, i| s.colorize(colors[i]) }.join("\n")
-end
-
-def get_service_status(service, url)
-    online = Net::Ping::HTTP.new(url).ping?
-    str = "#{service}:".ljust(15, ' ')
-    str += online ? 'Online'.colorize(:light_green) : 'Offline'.colorize(:light_red)
-    return str
+def get_horoscope
+    date = Date.today
+    key_path = File.dirname(__FILE__) + '/costar_api_key'
+    return 'Could not get horoscope...' unless File.exists?(key_path)
+    bearer_token = File.read(key_path).chomp
+    headers = { Authorization: bearer_token }
+    res = get("https://api.costarastrology.com/user/current/timeline/v1/daily/#{date.strftime("%Y-%m-%d")}T00:00:00Z", {}, headers)
+    return JSON.parse(res.body)['today']['pushNotificationText']
 end
 
 def get_weather(q)
@@ -78,7 +47,8 @@ def get_weather(q)
     return Array.new(3) unless File.exists?(key_path)
     # Get relevant data with API key
     appid = File.read(key_path).chomp
-    data = get_http_data("#{url}?appid=#{appid}&q=#{q}&units=metric")
+    res = get(url, { appid: appid, q: q, units: 'metric' })
+    data = JSON.parse(res.body)
     # Return if no API data received
     return Array.new(3) if data == {}
     # Return weather data
@@ -89,54 +59,28 @@ def get_weather(q)
     wind_speed = data["wind"]["speed"].round(2).to_s + 'kmph'
     time = Time.at(data["dt"]).to_datetime.strftime('%H:%M %p')
     thumb, color = case weather
-        when 'Thunderstorm', 'Drizzle', 'Rain'; [["' '' ' '", "'' ' ' '", "' ' '' '"], :light_blue]
-        when 'Snow'; [["* '* ' *", "'* ' * '", "*' * ' *"], :white]
-        when 'Clouds'; [["  .   * ", "*    . O", ". . *  ."], :dark_white]
-        else; [["  .   * ", "*    . O", ". . *  ."], :light_magenta]
+        when 'Thunderstorm', 'Drizzle', 'Rain'
+            [["' '' ' '", "'' ' ' '", "' ' '' '"], :light_blue]
+        when 'Snow'
+            [["* '* ' *", "'* ' * '", "*' * ' *"], :white]
+        when 'Clouds'
+            [["  .   * ", "*    . O", ". . *  ."], :dark_white]
+        else
+            [["  .   * ", "*    . O", ". . *  ."], :light_magenta]
     end
     return [
-        [thumb[0].ljust(10, ' '), description.ljust(22, ' '), q.titleize].join.colorize(color),
-        [thumb[1].ljust(10, ' '), temperature.ljust(22, ' '), "feels like #{feels_like}"].join.colorize(color),
-        [thumb[2].ljust(10, ' '), time.ljust(22, ' '), "wind speed #{wind_speed}"].join.colorize(color),
+        [thumb[0].ljust(10, ' '), description.ljust(20, ' '), q.titleize].join.colorize(color),
+        [thumb[1].ljust(10, ' '), temperature.ljust(20, ' '), "feels like #{feels_like}"].join.colorize(color),
+        [thumb[2].ljust(10, ' '), time.ljust(20, ' '), "wind speed #{wind_speed}"].join.colorize(color),
     ]
 end
 
-LOCATIONS = ['southampton', 'oxford']
-weathers = LOCATIONS.map { |q| get_weather(q) }.flatten
-websites = [
-    ['haedi.org', 'https://haedi.org/'],
-    ['milkbox.club', 'https://milkbox.club/'],
-    ['machin.dev', 'https://machin.dev/'],
-    ['vault', 'https://vault.machin.dev/'],
-    ['docs', 'https://docs.machin.dev/'],
-].map { |q| get_service_status(*q) }
-services = [
-    ['haedi API', 'https://haedi.org/api/ping'],
-    ['milkbox API', 'https://milkbox.club/api/ping'],
-    ['mindful API', 'https://mindful.machin.dev/api/ping'],
-].map { |q| get_service_status(*q) }
+weathers = ['southampton', 'bordeaux'].map { |q| get_weather(q) }.flatten
 
-# Operating system
-# print '  ' + get_platform() + "\n"
-# print "\n  ╒═══════════════════════════════════════════════════════╕\n"
-# Logo
-#print get_logo()
 print "\n"
-# Weather
-unless weathers.first == nil
-    print "  ╒═ Weather ═════════════════════════════════════════════╕\n\n"
-    print weathers.map.with_index { |line, i| '    ' + line + (i % 3 == 2 ? "\n" : "") }.join("\n")
-    print "\n"
-end
-
-print "  ╞═ Websites ═══════════════╡ ╞═ Services ═══════════════╡\n\n"
-(0...[websites.length, services.length].max + 2).each do |index|
-    website = (websites.length > index ? '  ' + websites[index] : '').ljust(43, ' ')
-    service = (services.length > index ? '  ' + services[index] : '')
-    website = '╘══════════════════════════╛' if index == websites.length + 1
-    service = '╘══════════════════════════╛' if index == services.length + 1
-    # puts [website, service].inspect
-    print '  ' + website + service + "\n"
-end
-
+print "  Welcome back...\n".italics
+print "\n"
+print weathers.map.with_index { |line, i| '     ' + line + (i % 3 == 2 ? "\n" : "") }.join("\n") unless weathers.first == nil
+print "\n"
+print "  " + get_horoscope.italics + "\n"
 print "\n"
