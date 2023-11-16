8646970bd1bef543d071e2b8fb5582d180add28c
louis <louis@machin.dev>
2023-11-16 10:46:51 +0000

    Updated MOTD

--- scripts/motd.rb
+++ scripts/motd.rb
@@ -30,23 +30,8 @@ def get(url, params = {}, headers = {})
     return res
 end
 
-def get_horoscope
-    date = Date.today
-    key_path = File.dirname(__FILE__) + '/costar_api_key'
-    return 'Could not get horoscope...' unless File.exists?(key_path)
-    bearer_token = File.read(key_path).chomp
-    headers = { Authorization: bearer_token }
-    res = get("https://api.costarastrology.com/user/current/timeline/v1/daily/#{date.strftime("%Y-%m-%d")}T00:00:00Z", {}, headers)
-    return JSON.parse(res.body)['today']['pushNotificationText']
-end
-
-def get_aws_bill_summary
-    res = get("https://machin.dev/raw/aws_billing_summary")
-    begin
-      return JSON.parse(res.body)
-    rescue
-      return nil
-    end
+def get_tidbit(resource)
+    return get('https://machin.dev/tidbit/' + resource).body
 end
 
 def get_weather(q)
@@ -85,19 +70,13 @@ def get_weather(q)
 end
 
 weathers = ['southampton', 'bordeaux'].map { |q| get_weather(q) }.flatten
-aws_bill = get_aws_bill_summary
-if aws_bill
-  aws_spend = aws_bill["GetCostAndUsage"]["ResultsByTime"][0]["Groups"][0]["Metrics"]["BlendedCost"]["Amount"].to_f.round(2)
-  aws_unit = aws_bill["GetCostAndUsage"]["ResultsByTime"][0]["Groups"][0]["Metrics"]["BlendedCost"]["Unit"]
-  aws_forecast = aws_bill["GetCostForecast"]["Total"]["Amount"].to_f.round(2)
-end
 
 print "\n"
 print "  Welcome back...\n".italics
 print "\n"
 print weathers.map.with_index { |line, i| '     ' + line + (i % 3 == 2 ? "\n" : "") }.join("\n") unless weathers.first == nil
 print "\n"
-print "  Monthly AWS spend: #{aws_spend}#{aws_unit} (#{aws_forecast}#{aws_unit} forecasted)\n" 
+print "  Monthly AWS spend: #{get_tidbit('aws_balance')} (#{get_tidbit('aws_forecast')} forecasted)\n" 
 print "\n"
-print "  " + get_horoscope.italics + "\n"
+print "  " + get_tidbit('horoscope').italics + "\n"
 print "\n"
